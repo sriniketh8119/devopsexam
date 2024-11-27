64b1d4be124f097bea1b12ad82d28c4d51a3c39b
sriniketh8119 <srinikethsharma@gmail.com>
2024-11-27 14:45:00 +0530

    commit 5

--- prime.java
+++ prime.java
@@ -1,6 +1,6 @@
 public class prime {
     public static void main(String[] args) {
-        int num = 4;
+        int num = 5;
         boolean isPrime = true;
 
         for (int i = 2; i <= num / 2; i++) {
